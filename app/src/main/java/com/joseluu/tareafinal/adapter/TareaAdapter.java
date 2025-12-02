8ce9fe9d114e4486ea5aeccf30d3f3d6349fdd99
José Luis Fuentes Parra <josefnts3115@gmail.com>
2025-12-02 09:35:56 +0100

    Refactor TareaAdapter variable names for clarity
    Renamed the internal ArrayList from 'datos' to 'tareaData' in TareaAdapter for improved readability. Also made TareaViewHolder static and simplified its instantiation.

--- app/src/main/java/com/joseluu/tareafinal/adapter/TareaAdapter.java
+++ app/src/main/java/com/joseluu/tareafinal/adapter/TareaAdapter.java
@@ -15,36 +15,35 @@ import com.joseluu.tareafinal.model.Tarea;
 import java.util.ArrayList;
 
 public class TareaAdapter extends RecyclerView.Adapter<TareaAdapter.TareaViewHolder>{
-    private final ArrayList<Tarea> datos;
+    private final ArrayList<Tarea> tareaData;
 
-    public TareaAdapter(ArrayList<Tarea> datos) {
-        this.datos = datos;
+    public TareaAdapter(ArrayList<Tarea> tareaData) {
+        this.tareaData = tareaData;
     }
 
     @NonNull
     @Override
     public TareaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.listitemcapital,parent,false);
-        TareaViewHolder tareaViewHolder = new TareaViewHolder(item);
-        return tareaViewHolder;
+        return new TareaViewHolder(item);
     }
 
     @Override
     public void onBindViewHolder(@NonNull TareaViewHolder holder, int position) {
         //Asignamos el dato del array correspondiente a la posición actual al
         //objeto ViewHolder, de forma que se represente en el RecyclerView.
-        holder.bindCapital(datos.get(position));
+        holder.bindCapital(tareaData.get(position));
     }
 
     @Override
     public int getItemCount() {
         //Devolvemos el tamaño de array de datos de Capitales
-        return datos.size();
+        return tareaData.size();
     }
 
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    public class TareaViewHolder extends RecyclerView.ViewHolder{
+    public static class TareaViewHolder extends RecyclerView.ViewHolder{
 
         private TextView titleTextView;
         private TextView habitantesTextView;
