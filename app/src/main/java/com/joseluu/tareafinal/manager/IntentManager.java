b049b6f28bdd7751de32e16a8b3ce7a30c427d1f
José Luis Fuentes Parra <josefnts3115@gmail.com>
2025-12-02 09:00:54 +0100

    Add IntentManager utility class
    Introduces IntentManager with a method to start activities using intents. This utility aims to simplify activity transitions within the app.

---
+++ app/src/main/java/com/joseluu/tareafinal/manager/IntentManager.java
@@ -0,0 +1,16 @@
+package com.joseluu.tareafinal.manager;
+
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntentManager {
+
+    public void startActivityWithIntent(AppCompatActivity appCompatActivity, AppCompatActivity appCompatActivity1){
+        try {
+            Intent intent = new Intent(appCompatActivity.getClass().newInstance(), appCompatActivity1.getClass());
+        } catch (IllegalAccessException | InstantiationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
